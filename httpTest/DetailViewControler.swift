fc0bce3000b1743944e1019c2b4e9cbe9ffe282f
dvltav <dominik@Dominiks-iMac.local>
2015-05-25 16:59:45 -0700

    init

---
+++ httpTest/DetailViewControler.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewControler.swift
+//  httpTest
+//
+//  Created by Dominik on 4/9/15.
+//  Copyright (c) 2015 Dominik. All rights reserved.
+//
+
+import Foundation
--- httpTest/ViewController.swift
+++ httpTest/ViewController.swift
@@ -9,12 +9,107 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet  var tempDisplay: UILabel!
+    
+    @IBOutlet var windSpeed: UILabel!
+    
+    @IBOutlet var lastUpdated: UILabel!
+    
+    @IBOutlet var slide: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg.png")!)
         // Do any additional setup after loading the view, typically from a nib.
+        getWeather(0)
+        
+    }
+    
+
+    
+
+    @IBAction func get24Weather(sender: UIButton) {
+        getWeather(144)
+        slide.value = 48
     }
 
+    
+    @IBAction func refresh(sender: UIButton) {
+        getWeather(0)
+        slide.value = 0
+    }
+  
+    
+    @IBAction func slider(sender: UISlider) {
+        var i = Int(sender.value)
+        
+        println("Slider valude = \(i)")
+        getWeather(i*3)
+        
+    }
+    
+    
+    func getWeather(offset : Int) {
+    
+        
+        var url2 = "http://vltavsky.com:500/weatherJson.php"
+        
+        let myUrl = NSURL(string: url2);
+        let request = NSMutableURLRequest(URL:myUrl!);
+        request.HTTPMethod = "POST";
+        
+        // Compose a query string
+        let postString = "offset=" + String(offset);
+      
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
+  
+        
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil
+            {
+                println("error=\(error)")
+                return
+            }
+            
+            // You can print out response object
+            println("response = \(response)")
+            
+            // Print out response body
+            let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("responseString = \(responseString)")
+            
+            //Let's convert response sent from a server side script to a NSDictionary object:
+            self.tempDisplay.text = "0"
+            
+
+            
+            var error: NSError? = nil;
+            var jsonObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.allZeros, error: &error)
+            
+            var date = (jsonObject as! NSDictionary)["date"] as! String
+            var outTemp = (jsonObject as! NSDictionary)["outTemp"]as! Double
+            var windGust = (jsonObject as! NSDictionary)["windGust"] as! Double
+            println("date: \(date)")
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                self.tempDisplay.text = (NSString(format: "%.1f", outTemp) as String) + " º"
+                self.windSpeed.text = (NSString(format: "%.0f", windGust) as String) + " mph"
+                self.lastUpdated.text = date
+            })
+            
+        }
+        
+        task.resume()
+
+    }
+    
+
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
