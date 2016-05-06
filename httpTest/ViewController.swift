5cd745b682caba604af16dd49d82ee4df39b513c
dvltav <dominik@dominiks-macbook-pro.local>
2016-05-06 14:36:34 -0700

    clean up

--- httpTest/ViewController.swift
+++ httpTest/ViewController.swift
@@ -45,11 +45,11 @@ class ViewController: UIViewController {
         let i = Int(sender.value)
         
         print("Slider valude = \(i)")
-        getWeather2(i*3)
+        getWeather(i*3)
         
     }
     
-    func getWeather2(offset : Int) {
+    func getWeather(offset : Int) {
         
         let myURL = NSURL(string: "http://bigpi.info:500/weatherJson.php")
         let request = NSMutableURLRequest(URL: myURL!)
@@ -81,84 +81,10 @@ class ViewController: UIViewController {
                 } //do
             } //let
         }//task
-        
+
         task.resume()
         
     }
-    
-    func getWeather(offset : Int) {
-    
-        
-        let url2 = "http://bigpi.info:500/weatherJson.php"
-        
-        let myUrl = NSURL(string: url2);
-        let request = NSMutableURLRequest(URL:myUrl!);
-        request.HTTPMethod = "POST";
-        
-        // Compose a query string
-        let postString = "offset=" + String(offset);
-      
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
-  
-        
-        
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-        data, response, error in
-            
-            if error != nil
-            {
-                print("error=\(error)")
-                return
-            }
-            
-            // You can print out response object
-            print("response = \(response)")
-            
-            // Print out response body
-            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
-            print("responseString = \(responseString)")
-            
-            //Let's convert response sent from a server side script to a NSDictionary object:
-            self.tempDisplay.text = "0"
-            
-
-            
-            var error: NSError? = nil;
-            var jsonObject: AnyObject?
-            do {
-                jsonObject = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions())
-            } catch var error1 as NSError {
-                error = error1
-                jsonObject = nil
-            } catch {
-                fatalError()
-            }
-            
-            var date = (jsonObject as! NSDictionary)["date"] as! String
-            var outTemp = (jsonObject as! NSDictionary)["outTemp"]as! Double
-            var windGust = (jsonObject as! NSDictionary)["windGust"] as! Double
-            print("date: \(date)")
-            
-            dispatch_async(dispatch_get_main_queue(), {
-                self.tempDisplay.text = (NSString(format: "%.1f", outTemp) as String) + " º"
-                self.windSpeed.text = (NSString(format: "%.0f", windGust) as String) + " mph"
-                self.lastUpdated.text = date
-            })
-            
-        }
-        
-        task.resume()
-
-    }
-    
-
-
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
